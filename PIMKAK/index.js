a82501b2bdb4b03d8db79efaeaeb79f39fbed7d1
AdxiveDay <kaokongxddd@gmail.com>
2024-08-07 22:13:59 +0700

    Add files via upload

---
+++ PIMKAK/index.js
@@ -0,0 +1,41 @@
+let button1 = document.getElementById("num1")
+let hiddentext1 = document.getElementById("hiddentext1")
+
+button1.addEventListener('click', () =>
+    hiddentext1.innerHTML = "Hello, Agent 3628. You have a new mission.Your task is to obtain the confidential Big Data of our rival company XX97 and deliver it to our organization within 10 minutes from now.Good luck.<br>สวัสดีเอเจนท์ 3628 ตอนนี้คุณได้รับภารกิจใหม่ หน้าที่ของคุณคือต้องนำข้อมูลลับที่เป็น Big Dataของบริษัทคู่แข่ง xx97 มาให้กับทางองค์กรภายใน 10 นาทีนับจากนี้ไป"
+)
+
+let button2 = document.getElementById("num2")
+let hiddentext2 = document.getElementById("hiddentext2")
+
+button2.addEventListener('click', () =>
+    hiddentext2.innerHTML = "All of you within the group must cooperate. We will have a mission envelope hiddenunder the desk or chair of one of you. You must retrieve it carefully and without making anynoise.<br>พวกคุณทุกคนภายในกลุ่มจะต้องร่วมมือกันเราจะมีซองภารกิจให้คุณซึ่งถูกซ่อนไว้ตามโต๊ะและเก้าอี้ของคุณคนใดคนนึงคุณต้องหยิบมันขึ้นมาด้วยความระมัดระวัง และไม่ควรส่งเสียงเป็นอันขาด"
+)
+
+let button3 = document.getElementById("num3")
+let hiddentext3 = document.getElementById("hiddentext3")
+
+button3.addEventListener('click', () =>
+    hiddentext3.innerHTML = "Once you have retrieved the hidden envelope, you must open it slowly. Inside, you will find confidential information about our rival company and two white envelopes.<br>เมื่อคุณได้ซองเอกสารลับที่ถูกซ่อนไว้แล้ว คุณจะต้องเปิดซองออกอย่างช้าๆ โดยภายในซองคุณจะพบกับข้อมูลลับมากมายของบริษัทคู่แข่ง และซองสีขาว 2 ซอง"
+)
+
+let button4 = document.getElementById("num4")
+let hiddentext4 = document.getElementById("hiddentext4")
+
+button4.addEventListener('click', () =>
+    hiddentext4.innerHTML = "Your task is to sort the numerous pieces of information found and categorize them into: 1. Big Data 2. Not Big Data.<br>หน้าที่ของคุณคือคัดเลือกข้อมูลมากมายที่พบและแบ่งออกเป็น 1. ข้อมูลที่เป็น Big Data 2.ข้อมูลที่ไม่เป็น Big Data"
+)
+
+let button5 = document.getElementById("num5")
+let hiddentext5 = document.getElementById("hiddentext5")
+
+button5.addEventListener('click', () =>
+    hiddentext5.innerHTML = "Once you have sorted the information, place the data into the correct white envelopes. When you have finished, send one member of your team, who you think is the smartest andmost handsome, to meet us at the...S E C R E T T A B L E... We hope your team member will find thesecret table and deliver the documents on time.<br>เมื่อคุณคัดเลือกข้อมูลของคุณครบแล้ว ให้คุณนำข้อมูลใส่ตามซองสีขาวให้ถูกต้องและเมื่อคุณนำข้อมูลใส่ซองเสร็จแล้ว ให้คุณส่งตัวแทน 1 คนของทีมคุณที่คิดว่าฉลาดที่สุด หล่อเท่ที่สุดมาหาเราที่....โ ต๊ ะ ลั บ.....เราหวังว่าสมาชิกของคุณจะหาโต๊ะลับเจอและส่งเอกสารได้ทันเวลา"
+)
+
+let button6 = document.getElementById("num6")
+let hiddentext6 = document.getElementById("hiddentext6")
+
+button6.addEventListener('click', () =>
+    hiddentext6.innerHTML = "Good luck.<br>ขอให้โชคดี."
+)
